8c7ce4e9a903da6c3b082b35400eaaf713d09463
ChloeArd <chloe.ardoise@gmail.com>
2022-02-21 11:38:39 +0100

    defi + test

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -10,19 +10,13 @@ class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
     public function index(): Response {
-        return $this->render('home/index.html.twig', [
-            'user' => ["Chloé", "Charlotte", "Didier", "Cassandra", "Gerard"],
-            'number' => [43, 8, 35, 8, 75, 41, 55, 75, 39, 20, 15, 1],
-            "animals" => [
-                1 => "chien",
-                2 => "chat",
-                3 => "lapin"
-            ]
-        ]);
+        return $this->render('home/index.html.twig');
     }
 
     #[Route('/test', name: 'test')]
     public function test(): Response {
+        $this->addFlash("success", "Ca fonctionne correctement !");
+
         return $this->render('home/test.html.twig', [
             'user' => ["Chloé", "Charlotte", "Didier", "Cassandra", "Gerard"],
             'number' => [43, 8, 35, 8, 75, 41, 55, 75, 39, 20, 15, 1],
