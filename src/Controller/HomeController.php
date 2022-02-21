b984392557bf8b80aa1c84006e20d51175f90036
ChloeArd <chloe.ardoise@gmail.com>
2022-02-21 11:32:45 +0100

    defi + test

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/', name: 'home')]
+    public function index(): Response {
+        return $this->render('home/index.html.twig', [
+            'user' => ["Chloé", "Charlotte", "Didier", "Cassandra", "Gerard"],
+            'number' => [43, 8, 35, 8, 75, 41, 55, 75, 39, 20, 15, 1],
+            "animals" => [
+                1 => "chien",
+                2 => "chat",
+                3 => "lapin"
+            ]
+        ]);
+    }
+
+    #[Route('/test', name: 'test')]
+    public function test(): Response {
+        return $this->render('home/test.html.twig', [
+            'user' => ["Chloé", "Charlotte", "Didier", "Cassandra", "Gerard"],
+            'number' => [43, 8, 35, 8, 75, 41, 55, 75, 39, 20, 15, 1],
+            "animals" => [
+                1 => "chien",
+                2 => "chat",
+                3 => "lapin"
+            ]
+        ]);
+    }
+}
