47589f25d822285ef8ead4a9022d0ce99a8ac096
ChloeArd <chloe.ardoise@gmail.com>
2022-02-21 15:14:40 +0100

    defi + test

--- assets/app.js
+++ assets/app.js
@@ -7,6 +7,11 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
+import './styles/style.css';
 
-// start the Stimulus application
-import './bootstrap';
+// Import d'images
+import './img/image1.png';
+import './img/image2.png'
+
+// Import de fichier JS
+import './js/app';
--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,13 @@ if (!Encore.isRuntimeEnvironmentConfigured()) {
 Encore
     // directory where compiled assets will be stored
     .setOutputPath('public/build/')
+
+    .copyFiles({
+        from: "./assets/images-statiques",
+        to: "static/[path][name].[ext]",
+        pattern: /\.(png|jpg|jpeg)$/
+    })
+
     // public path used by the web server to access the output path
     .setPublicPath('/build')
     // only needed for CDN's or sub-directory deploy
@@ -21,9 +28,10 @@ Encore
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/app.js')
-
+    .addEntry("admin", "./assets/admin.js")
+    .addStyleEntry("particularity", "./assets/styles/particularity.css")
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
-    .enableStimulusBridge('./assets/controllers.json')
+    //.enableStimulusBridge('./assets/controllers.json')
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
@@ -56,10 +64,10 @@ Encore
     })
 
     // enables Sass/SCSS support
-    //.enableSassLoader()
+    .enableSassLoader()
 
     // uncomment if you use TypeScript
-    //.enableTypeScriptLoader()
+    .enableTypeScriptLoader()
 
     // uncomment if you use React
     //.enableReactPreset()
